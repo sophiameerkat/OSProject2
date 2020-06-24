d65f8554d7ff76d4a426655d9e90040f0eedcc72
Grace Theodore <45305270+gracetheo@users.noreply.github.com>
2020-06-24 19:32:17 +0800

    Update fcntl_slave2.sh

--- demo/fcntl_slave2.sh
+++ demo/fcntl_slave2.sh
@@ -5,8 +5,8 @@ cd ../user_program
 echo "Demo fcntl sample_input_2"
 cd ../output/sample_output_2
 sudo ../../user_program/slave 1 fcntl_received_file fcntl 127.0.0.1
-diff -a fcntl_received_file ../../input/sample_input_2/target_file > /dev/null 2>&1
+diff -a fcntl_received_file_1 ../../input/sample_input_2/target_file_1 > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
-  echo "target_file and fcntl_received_file are similar."
+  echo "target_file_1 and fcntl_received_file_1 are similar."
 fi
