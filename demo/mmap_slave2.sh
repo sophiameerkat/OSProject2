ea39b92a206feae2a128d001a276ba6cbb590413
Grace Theodore <45305270+gracetheo@users.noreply.github.com>
2020-06-24 19:32:56 +0800

    Update mmap_slave2.sh

--- demo/mmap_slave2.sh
+++ demo/mmap_slave2.sh
@@ -5,8 +5,8 @@ cd ../user_program
 echo "Demo mmap sample_input_2"
 cd ../output/sample_output_2
 sudo ../../user_program/slave 1 mmap_received_file mmap 127.0.0.1
-diff -a mmap_received_file ../../input/sample_input_2/target_file > /dev/null 2>&1
+diff -a mmap_received_file_1 ../../input/sample_input_2/target_file_1 > /dev/null 2>&1
 if [ $? -eq 0 ]
 then
-  echo "target_file and mmap_received_file are similar."
+  echo "target_file_1 and mmap_received_file_1 are similar."
 fi
