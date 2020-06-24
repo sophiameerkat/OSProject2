4de25fd97d36724a6c799f9fa6f22b5fc4c00521
Grace Theodore <45305270+gracetheo@users.noreply.github.com>
2020-06-24 19:31:05 +0800

    Update mmap_master2.sh

--- demo/mmap_master2.sh
+++ demo/mmap_master2.sh
@@ -4,4 +4,10 @@ cd ../user_program
 
 echo "Demo mmap sample_input_2"
 cd ../input/sample_input_2
-sudo ../../user_program/master 1 target_file mmap #>> result_mmap_2
+sudo ../../user_program/master 1 target_file mmap
+
+echo "dmesg results"
+dmesg | grep master:
+dmesg | grep slave:
+dmesg | grep master: >> ../../dmesg/mmap_master_2
+dmesg | grep -c slave: >> ../../dmesg/mmap_slave_2
